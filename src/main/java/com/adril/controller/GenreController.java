4a5cd28fa2f78e1d5e82c4e2287406bfe0ff4257
= <ajla.dzekovic.2470@metropolitan.ac.rs>
2017-01-28 03:18:40 +0100

    Praise me

--- src/main/java/com/adril/controller/GenreController.java
+++ src/main/java/com/adril/controller/GenreController.java
@@ -6,11 +6,17 @@
 package com.adril.controller;
 
 import com.adril.dao.GenreDao;
+import com.adril.entity.Genre;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -23,6 +29,40 @@ public class GenreController {
     
     @RequestMapping(value = "/addGenre", method = RequestMethod.GET)
     public String addGenre(Model model){
+        model.addAttribute("genre", new Genre());
+        List genres = genreDao.getGenreList();
+        model.addAttribute("genres", genres);
         return "addGenre";
+    }
+    
+    @RequestMapping(value = "addGenre", method = RequestMethod.POST)
+    public ModelAndView addGenre(@ModelAttribute("genre") Genre genre, ModelAndView model){
+        genre = genreDao.addGenre(genre);
+        model.addObject("successMsg", "Genre Added!");
+        model.addObject("genre", new Genre());
+        List genres = genreDao.getGenreList();
+        model.addObject("genres", genres);
+        return model;
+    }
+    
+    @RequestMapping(value = "editGenre/{id}", method = RequestMethod.GET)
+    public String editGenre(@PathVariable("id") int id, Model model){
+        Genre genre = genreDao.getGenreById(id);
+        model.addAttribute("genre", genre);
+        List genres = genreDao.getGenreList();
+        model.addAttribute("genres", genres);
+        return "addGenres";
+    }
+    
+    @RequestMapping(value = "deleteGenre/{id}", method = RequestMethod.GET)
+    public String deleteGenre(@PathVariable("id") int id, HttpServletRequest request){
+        Genre genre = genreDao.getGenreById(id);
+        if (genre == null){
+            String referer = request.getHeader("Referer");
+            return "redirect:" + referer;
+        }
+        genreDao.deleteGenre(genre);
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
     }
 }
--- src/main/webapp/WEB-INF/jsp/addGenre.jsp
+++ src/main/webapp/WEB-INF/jsp/addGenre.jsp
@@ -10,6 +10,48 @@
 <%@ taglib uri='http://java.sun.com/jsp/jstl/core' prefix='c'%>
 
 <%@include file="partials/header.jsp" %>
-<h1>Do you ever get a feeling of deja vu?</h1>
+<h1>Add Genre</h1>
+<div class="container">
+    <div class="col-md-6 col-md-offset-3">
+        <c:url var="dodajGenre" value="/addGenre" ></c:url>
+        <form:form method="POST" action="${addGenre}" modelAttribute="genre">
+            <% String success = (String) request.getAttribute("successMsg");%>
+            <%= (success != null) ? "<div class=\"alert alert-success\">" + success + "</div>" : ""%>
+            <div class="form-group">
+                <form:label path="name">
+                    Game Name
+                </form:label>
+                <form:input type="name" class="form-control" id="name" placeholder="Game name" path="name" />
+            </div>
+
+            <form:input type="hidden" id="id" class="form-control" placeholder="id" path="id" />
+            <button type="submit" class="btn btn-primary">Add</button>
+        </form:form>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+        <c:if test="${!empty genres}">
+
+            <table class="table table-striped mojatabela">
+                <thead>
+                    <tr>
+                        <th>Game Name</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody> 
+                    <c:forEach items="${genres}" var="gens">
+
+                        <tr>
+                            <td>${gens.name}</td>
+                            <td><a href="<c:url value='/editGenre/${gens.id}' />">edit</a></td>
+                            <td><a href="<c:url value='/deleteGenre/${gens.id}' />">delete</a></td>
+                        </tr>
+                    </c:forEach>
+                </tbody>
+            </table>
+        </c:if>
+    </div>    
+</div>
 
 <%@include file="partials/footer.jsp" %>
